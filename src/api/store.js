3b822926c1e59bf30fb34a0ab8982369517b7704
Jo Seunghwan <shj95@korea.ac.kr>
2020-05-31 22:40:46 +0900

    phase 설정

--- src/api/store.js
+++ src/api/store.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create();
+axiosClient.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
 
 export async function getStoresByLatLng({ lat, lng }) {
 	return await axiosClient.get(`/stores/nearby?latitude=${lat}&longitude=${lng}`);
