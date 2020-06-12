9fe1c686bc2470aa2669cd7bb0f32d53bea3fb0c
Jo Seunghwan <shj95@korea.ac.kr>
2020-06-12 13:10:02 +0900

    HTTPS 하드 코딩

--- src/api/store.js
+++ src/api/store.js
@@ -6,10 +6,12 @@ axiosClient.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
 export async function getStoresByLatLng({ lat, lng, diameter }) {
 	// XXX : API에 위경도가 뒤집혀있음
 	return await axiosClient.get(
-		`/stores/nearby?latitude=${lng}&longitude=${lat}&diameter=${Math.ceil(diameter)}`,
+		`https://api.kkumtree.xyz/stores/nearby?latitude=${lng}&longitude=${lat}&diameter=${Math.ceil(
+			diameter,
+		)}`,
 	);
 }
 
 export async function getStoreById(id) {
-	return await axiosClient.get(`/stores/${id}`);
+	return await axiosClient.get(`https://api.kkumtree.xyz/stores/${id}`);
 }
