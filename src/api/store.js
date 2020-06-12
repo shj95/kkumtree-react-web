3ef85a395da728e77f35ee5a35f806e5e4af4dbd
Jo Seunghwan <shj95@korea.ac.kr>
2020-06-12 13:04:19 +0900

    CORS 해결

--- src/api/store.js
+++ src/api/store.js
@@ -6,14 +6,10 @@ axiosClient.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
 export async function getStoresByLatLng({ lat, lng, diameter }) {
 	// XXX : API에 위경도가 뒤집혀있음
 	return await axiosClient.get(
-		`https://cors-anywhere.herokuapp.com/http://api.kkumtree.xyz/stores/nearby?latitude=${lng}&longitude=${lat}&diameter=${Math.ceil(
-			diameter,
-		)}`,
+		`/stores/nearby?latitude=${lng}&longitude=${lat}&diameter=${Math.ceil(diameter)}`,
 	);
 }
 
 export async function getStoreById(id) {
-	return await axiosClient.get(
-		`https://cors-anywhere.herokuapp.com/http://api.kkumtree.xyz/stores/${id}`,
-	);
+	return await axiosClient.get(`/stores/${id}`);
 }
